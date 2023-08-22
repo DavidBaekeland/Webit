7c06f6dd873546cdd5a769e538ae5bb75827daf9
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-08-22 12:49:15 +0200

    Add: start and end attibutes in Auction model
    Modified start_date en end_time to show dates on peronal card

--- app/Models/Auction.php
+++ app/Models/Auction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -18,12 +19,30 @@ class Auction extends Model
         'img'
     ];
 
+
     public function getRouteKeyName()
     {
         return 'slug';
     }
 
-    /* Relationships */
+    /** Attibutes */
+    public function getStartAttribute()
+    {
+        $startDate = Carbon::createFromFormat('Y-m-d H:i:s', $this->start_date)->format('d-m-Y');
+        $startTime = Carbon::createFromFormat('Y-m-d H:i:s', $this->start_date)->format('H:i');
+
+        return $startDate." om ". $startTime;
+    }
+
+    public function getEndAttribute()
+    {
+        $endDate = Carbon::createFromFormat('Y-m-d H:i:s', $this->end_date)->format('d-m-Y');
+        $endTime = Carbon::createFromFormat('Y-m-d H:i:s', $this->end_date)->format('H:i');
+
+        return $endDate." om ". $endTime;
+    }
+
+    /** Relationships */
     public function products(): HasMany
     {
         return $this->hasMany(Product::class);
