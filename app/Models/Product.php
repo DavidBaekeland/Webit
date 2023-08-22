a380016a15de0be603d066ff1863b8f4c28b5745
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-08-22 10:39:28 +0200

    Add: model + migration + relationship -> Image
    Added Image model with fillable and relationship (to product) + migrations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -33,4 +33,9 @@ class Product extends Model
     {
         return $this->hasMany(Offer::class);
     }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(Image::class);
+    }
 }
