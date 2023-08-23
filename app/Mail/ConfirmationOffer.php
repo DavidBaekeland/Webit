66f602973e232311c65e1e2879979b4cff05971e
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2023-08-23 18:12:48 +0200

    Add: encryption + default gueueable
    Added ShouldQueue and ShouldBeEncrypted interfaces to make Contact and ConfirmOffer encrypted and default queueable.

--- app/Mail/ConfirmationOffer.php
+++ app/Mail/ConfirmationOffer.php
@@ -5,13 +5,14 @@ namespace App\Mail;
 use App\Models\Offer;
 use App\Models\Product;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ConfirmationOffer extends Mailable
+class ConfirmationOffer extends Mailable implements ShouldQueue, ShouldBeEncrypted
 {
     use Queueable, SerializesModels;
 
--- app/Mail/Contact.php
+++ app/Mail/Contact.php
@@ -3,13 +3,14 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class Contact extends Mailable
+class Contact extends Mailable implements ShouldQueue, ShouldBeEncrypted
 {
     use Queueable, SerializesModels;
 
